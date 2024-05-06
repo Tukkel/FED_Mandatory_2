9f6bc08a4614e77cf52d685f665c367ba869c352
Tukkel <terkel1234@gmail.com>
2024-05-06 13:08:20 +0200

    Post Model now works

--- model-management/src/hooks/useModels.ts
+++ model-management/src/hooks/useModels.ts
@@ -2,20 +2,16 @@ import IModel from "../types/IModel";
 
 const PostModelUrl = "http://localhost:7181/api/Models";
 
-export function usePostModel() {
-  const postModel = (model: IModel) => {
-    fetch(PostModelUrl, {
-      method: "POST",
-      body: JSON.stringify(model),
-      credentials: "include",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json()) // Extract JSON data from response
-      .catch((error) => alert("Something bad happened: " + error));
-  };
-
-  return postModel;
+export function usePostModel(model: IModel) {
+  fetch(PostModelUrl, {
+    method: "POST",
+    body: JSON.stringify(model),
+    credentials: "include",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json()) // Extract JSON data from response
+    .catch((error) => alert("Something bad happened: " + error));
 }
