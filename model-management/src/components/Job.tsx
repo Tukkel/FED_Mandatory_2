17216b94a4edb69ac6e13c6959600e62e43ff0f1
Jonas <jona813k@hotmail.com>
2024-05-07 14:15:28 +0200

    and this it the way of wind

--- model-management/src/components/Job.tsx
+++ model-management/src/components/Job.tsx
@@ -20,8 +20,8 @@ const Job: React.FC<JobProps> = ({ job, onJobChanged }) => {
       <p>Location: {job.location}</p>
       <p>Comments: {job.comments}</p>
       <div className="m-2">
-          <ModelAddDialog jobId={job.jobId}/>
-        </div>
+        <ModelAddDialog jobId={job.jobId}/>
+      </div>
       <h3 className={"text-xl"}>Models on this job:</h3>
       {job.models &&
         job.models.map((model, modelIndex) => (
@@ -37,11 +37,6 @@ const Job: React.FC<JobProps> = ({ job, onJobChanged }) => {
               {model.firstName} {model.lastName}
             </Button>
           </div>
-             <Button variant="contained"  onClick={() => alert(`First Name: ${model.firstName}\nLast Name: ${model.lastName}\nPhone Number: ${model.phoneNo}\nEmail: ${model.email}`)} >
-    {model.firstName} {model.lastName}
-    </Button>
-  
-    </div>
         ))}
     </div>
   );
