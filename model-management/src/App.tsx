96d11f704e359258188b4c8f3210a3c4a7dceab7
Tukkel <terkel1234@gmail.com>
2024-05-08 14:03:50 +0200

    Routes are now protected

--- model-management/src/App.tsx
+++ model-management/src/App.tsx
@@ -1,8 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import DefaultPage from "./pages/DefaultPage";
 import LoginPage from "./pages/LoginPage";
 import JobsPage from "./pages/JobsPage";
 import ManageJobsPage from "./pages/ManageJobsPage";
+
+function ProtectedModelPage() {
+  const isAuthenticated = localStorage.getItem("role") === "Model";
+  return isAuthenticated ? <JobsPage /> : <Navigate to="/login" />;
+}
+
+function ProtectedManagerPage() {
+  const isAuthenticated = localStorage.getItem("role") === "Manager";
+  return isAuthenticated ? <ManageJobsPage /> : <Navigate to="/login" />;
+}
 
 function App() {
   return (
@@ -10,8 +20,8 @@ function App() {
       <Routes>
         <Route path="/" element={<DefaultPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/manageJobs" element={<ManageJobsPage />} />
+        <Route path="/jobs" element={<ProtectedModelPage />} />
+        <Route path="/manageJobs" element={<ProtectedManagerPage />} />
       </Routes>
     </div>
   );
